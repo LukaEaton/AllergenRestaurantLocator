b2c6a234925a1705bb8f6d958e45050c26ffe7f3
LukaEaton <eatonluka@gmail.com>
2024-07-20 03:51:03 -0400

    Initial setup (navbar and router pages)

---
+++ my-react-app/src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import { Link, Outlet } from 'react-router-dom'
+import '../styles/NavBar.css'
+
+function NavBar(){
+    return ( 
+        <>
+            <nav>
+                <ul className="links">
+                <li>
+                    <Link to="/">
+                        <svg className="w-[48px] h-[48px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
+                        </svg>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/resume">Resume</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                </ul>
+            </nav>
+            <main>
+                <Outlet/>
+            </main>
+        </>
+    )
+}
+
+export default NavBar
