caead6f656def16a2ff1f1be18f4752c0d43f7d6
LukaEaton <eatonluka@gmail.com>
2024-07-31 01:32:00 -0400

    server-side setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,48 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+require('dotenv').config();
+
+const chrome = require('selenium-webdriver/chrome');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const screen = {
+    width: 640,
+    heigh: 480
+}
+
+const url = 'https://www.allergyeats.com/';
+
+// Google Maps API Key
+// Geocoding API to get coordinates from address: https://developers.google.com/maps/documentation/geocoding
+// potentially use the pollen API
+const maps_api_key = process.env.MAPS_API_KEY;
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .build();
 
 app.get("/api", (req, res) => {
     res.json({test: ["this","is","a","test"]});
+});
+
+app.post('/search', (req, res) => {
+    // console.log(req.body);
+    driver.get(url).then( () => {
+        driver.findElements(By.xpath('//*[@id="home-search-criteria"]/div/form/div[1]/fieldset/div/label/input'))
+        .then( arr => {
+            for(let i = 0; i < arr.length; i++){
+                arr[i].getAttribute('value')
+                .then( val => {
+                    console.log(val);
+                });
+                // arr[i].click()
+                // .then( () => {
+                //     console.log('clicked');
+                // });
+            }
+        });
+    });
 });
 
 app.listen(8080, () => {
